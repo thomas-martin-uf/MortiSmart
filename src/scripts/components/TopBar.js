5aacb0b73c398a8cb456df97eed8d04cbbe65e76
thomas-martin-uf <thomas.martin@ufl.edu>
2023-09-26 14:13:40 -0400

    adding title to top bar

--- src/scripts/components/TopBar.js
+++ src/scripts/components/TopBar.js
@@ -6,6 +6,9 @@ export const TopBar = {
     getElement: () => {
         const div = document.createElement("div");
         div.classList.add("top-bar");
+        const h1 = document.createElement("h1");
+        h1.textContent = "MortiSmart";
+        div.appendChild(h1);
         return div;
     },
     render: (parent_id = DEFAULT_TOP_BAR_RENDER_CONTAINER_ID) => {
